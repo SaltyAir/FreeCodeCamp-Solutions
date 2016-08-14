615a996a51cf11032eef6d83ecbeb235a63efc63
SaltyAir <itsdake@gmail.com>
2016-08-14 13:31:02 +0200

    Update (two solutions)

--- Basic Algorithm Scripting/5. Title Case a Sentence.js
+++ Basic Algorithm Scripting/5. Title Case a Sentence.js
@@ -1,15 +1,34 @@
 function titleCase(str) {
-  var tmp;
-  var newStr = [];
-
+  var newString = [];
   str = str.toLowerCase().split(" ");
-
-  for (var i = 0; i < str.length; i++) {
-        tmp = str[i];
-        newStr[i] = tmp.charAt(0).toUpperCase() + tmp.slice(1);
-  }
-  return newStr.join(" ");
-
+  newString = str.map(charToUpperCase).join(" ");
+  return newString;
 }
 
-titleCase("I'm a little tea pot");
+function charToUpperCase(x) {
+  var origLetter = x.charAt(0);
+  var capital = origLetter.toUpperCase();
+  x = x.replace(origLetter, capital);
+  return x;
+}
+
+console.log(titleCase("I'm a little tea pot"));
+
+
+// 2nd solution:
+
+// function titleCase(str) {
+//   var tmp;
+//   var neueWoerter = [];
+//
+//   str = str.toLowerCase().split(" ");
+//
+//   for (var i = 0; i < str.length; i++) {
+//         tmp = str[i];
+//         neueWoerter[i] = tmp.charAt(0).toUpperCase() + tmp.slice(1);
+//   }
+//   return neueWoerter.join(" ");
+//
+// }
+//
+// console.log(titleCase("I'm a little tea pot"));
