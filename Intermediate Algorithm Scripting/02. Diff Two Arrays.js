4cac1dd0e9e3f89c3a322abbc429cefd11a269eb
SaltyAir <itsdake@gmail.com>
2016-08-20 14:12:30 +0200

    Complete

---
+++ Intermediate Algorithm Scripting/02. Diff Two Arrays.js
@@ -0,0 +1,16 @@
+function diffArray(arr1, arr2) {
+    var finalArr = [];
+    for (var i = 0; i < arr1.length; i++) {
+        if (arr2.indexOf(arr1[i]) === -1) {
+            finalArr.push(arr1[i]);
+        }
+    }
+
+    for (var j = 0; j < arr2.length; j++) {
+        if (arr1.indexOf(arr2[j]) === -1) {
+            finalArr.push(arr2[j]);
+        }
+    }
+    return finalArr;
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
